17465a301178a98190245750e2b32922369e1e6b
Ed Kellett <e@kellett.im>
2016-02-23 20:51:21 +0000

    really fix travis deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 if [ -z "$TRAVIS_BRANCH" ]; then exit; fi
 if [ ! "$TRAVIS_BRANCH" = master ]; then exit; fi
 
-export GIT_SSH_COMMAND="ssh -i $(readlink -f .deploy-key)"
+export SSH_KEYFILE="$(readlink -f .deploy-key)"
+export GIT_SSH="$(readlink -f ssh.sh)"
 git clone -b gh-pages git@github.com:freenode/web-7.0.git .deploy
 cd .deploy
 rm -rf *
