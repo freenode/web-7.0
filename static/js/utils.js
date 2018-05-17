b2e99802e9a3c2afee90c9b3454f1730503e972b
Svante Bengtson <svante.b37@gmail.com>
2018-05-17 11:36:54 +0200

    let not allowed in js when uglifying

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,11 +1,13 @@
+'use strict'
+
 /**
  * Removes soft hyphens from copied text
  * @param  {Object} event
  * @return {Undefined}
  */
 document.addEventListener('copy', function (event) {
-  let oldText = window.getSelection().toString()
-  let newText = oldText.replace(/\u00AD/g, '')
+  var oldText = window.getSelection().toString()
+  var newText = oldText.replace(/\u00AD/g, '')
 
   event.clipboardData.setData('text/plain', newText)
   event.clipboardData.setData('text/html', newText.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'))
